24f323227833bb80dd4fffcc2d291c16336cfa77
YusufBday <79852993+YusufBday@users.noreply.github.com>
2021-03-02 17:52:49 +0530

    Add files via upload

---
+++ javascript/script.js
@@ -0,0 +1,295 @@
+var cards = [
+  {
+    name: "cathy",
+    img: "../images/card/cathy.jpeg",
+    iframe: `<iframe width="1200" height="600" src="/videos/videoGame/cathy.mp4" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen ></iframe>`,
+    id: 1,
+  },
+  {
+    name: "dhar",
+    img: "../images/card/dhar.jpg",
+    iframe: `<iframe width="1200" height="600" src="/videos/videoGame/dhar.mp4" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen ></iframe>`,
+    id: 2,
+  },
+  {
+    name: "hema",
+    img: "../images/card/hema.jpeg",
+    iframe:`<iframe width="1200" height="600" src="/videos/videoGame/hema.mp4" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen ></iframe>`,
+    id: 3,
+  },
+  {
+    name: "kabi",
+    img: "../images/card/kabi.jpeg",
+    iframe: `<iframe width="1200" height="600" src="/videos/videoGame/kabi.mp4" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`,
+    id: 4,
+  },
+  {
+    name: "mak",
+    img: "../images/card/mak.jpg",
+    iframe: `<iframe width="1200" height="600" src="/videos/videoGame/mak.mp4" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`,
+    id: 5,
+  },
+  {
+    name: "aps",
+    img: "../images/card/aps.jpeg",
+    iframe: `<iframe width="1200" height="600" src="/videos/videoGame/aps.mp4" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`,
+    id: 6,
+  },
+  {
+    name: "harshi",
+    img: "../images/card/harshi.jpeg",
+    iframe: `<iframe width="1200" height="600" src="/videos/videoGame/harshi.mp4" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`,
+    id: 7,
+  },
+  {
+    name: "kasil",
+    img: "../images/card/kasil.jpeg",
+    iframe: `<iframe width="1200" height="600" src="/videos/videoGame/kasil.mp4" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`,
+    id: 8,
+  },
+  {
+    name: "mano",
+    img: "../images/card/mano.jpeg",
+    iframe: `<iframe width="1200" height="600" src="/videos/videoGame/mano.mp4" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`,
+    id: 9,
+  },
+  {
+    name: "natty",
+    img: "../images/card/natty.jpeg",
+    iframe: `<iframe width="1200" height="600" src="/videos/videoGame/natty.mp4" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`,
+    id: 10,
+  },
+  {
+    name: "naveen",
+    img: "../images/card/naveen.jpeg",
+    iframe: `<iframe width="1200" height="600" src="/videos/videoGame/naveen.mp4" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`,
+    id: 11,
+  },
+  {
+    name: "nithish",
+    img:"../images/card/nithish.jpeg",
+    iframe: `<iframe width="1200" height="600" src="/videos/videoGame/nithish.mp4" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`,
+    id: 12,
+  },
+  {
+    name: "rohit",
+    img:"../images/card/rohit.jpeg",
+    iframe: `<iframe width="1200" height="600" src="/videos/videoGame/rohit.mp4" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`,
+    id: 13,
+  },
+  {
+    name: "sandy",
+    img:"../images/card/sandy.jpeg",
+    iframe: `<iframe width="1200" height="600" src="/videos/videoGame/sandy.mp4" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`,
+    id: 14,
+  },
+  {
+    name: "siva",
+    img:"../images/card/siva.jpeg",
+    iframe: `<iframe width="1200" height="600" src="/videos/videoGame/siva.mp4" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`,
+    id: 15,
+  },
+  {
+    name: "sriju",
+    img: "../images/card/sriju.jpeg",
+    iframe: `<iframe width="1200" height="600" src="/videos/videoGame/sriju.mp4" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`,
+    id: 16,
+  },
+  {
+    name: "ssr",
+    img: "../images/card/ssr.jpeg",
+    iframe: `<iframe width="1200" height="600" src="/videos/videoGame/ssr.mp4" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`,
+    id: 17,
+  },
+  {
+    name: "swetha",
+    img:"../images/card/swetha.jpeg",
+    iframe: `<iframe width="1200" height="600" src="/videos/videoGame/swetha.mp4" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`,
+    id: 18,
+  },
+  {
+    name: "vijay",
+    img:"../images/card/vijay.jpeg",
+    iframe: `<iframe width="1200" height="600" src="/videos/videoGame/vijay.mp4" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`,
+    id: 19,
+  },
+  {
+    name: "vishnu",
+    img:"../images/card/vishnu.jpeg",
+    iframe: `<iframe width="1200" height="600" src="/videos/videoGame/vishnu.mp4" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`,
+    id: 20,
+  },
+  {
+    name: "kiran",
+    img:"../images/card/kiran.jpeg",
+    iframe: `<iframe width="1200" height="600" src="/videos/videoGame/kiran.mp4" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`,
+    id: 21,
+  },
+  {
+    name: "thar",
+    img:"../images/card/thar.jpeg",
+    iframe: `<iframe width="1200" height="600" src="/videos/videoGame/thar.mp4" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`,
+    id: 22,
+  },
+  {
+    name: "san",
+    img:"../images/card/san.jpeg",
+    iframe: `<iframe width="1200" height="600" src="/videos/videoGame/san.mp4" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`,
+    id: 23,
+  },
+  {
+    name: "yogesh",
+    img:"../images/card/yogesh.jpeg",
+    iframe: `<iframe width="1200" height="600" src="../videos/videoGame/yogesh.mp4" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`,
+    id: 24,
+  },
+  {
+    name: "dharika",
+    img:"../images/card/dharika.jpeg",
+    iframe: `<iframe width="1200" height="600" src="/videos/videoGame/dharika.mp4" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`,
+    id: 25,
+  },
+];
+
+(function () {
+  var Memory = {
+    init: function (cards) {
+      this.$game = $(".game");
+      this.$modal = $(".modal");
+      this.$overlay = $(".modal-overlay");
+      this.$restartButton = $("button.restart");
+      this.cardsArray = $.merge(cards, cards);
+      this.shuffleCards(this.cardsArray);
+      this.setup();
+    },
+
+    shuffleCards: function (cardsArray) {
+      this.$cards = $(this.shuffle(this.cardsArray));
+    },
+
+    setup: function () {
+      this.html = this.buildHTML();
+      this.$game.html(this.html);
+      this.$memoryCards = $(".card");
+      this.paused = false;
+      this.guess = null;
+      this.binding();
+    },
+
+    binding: function () {
+      this.$memoryCards.on("click", this.cardClicked);
+      this.$restartButton.on("click", $.proxy(this.reset, this));
+    },
+    // kinda messy but hey
+    cardClicked: function () {
+      var _ = Memory;
+      var $card = $(this);
+      if (
+        !_.paused &&
+        !$card.find(".inside").hasClass("matched") &&
+        !$card.find(".inside").hasClass("picked")
+      ) {
+        $card.find(".inside").addClass("picked");
+        if (!_.guess) {
+          _.guess = $(this).attr("data-id");
+        } else if (
+          _.guess == $(this).attr("data-id") &&
+          !$(this).hasClass("picked")
+        ) {
+          $(".picked").addClass("matched");
+          openModal(_.guess);
+          _.guess = null;
+          // add here
+
+          // alert("hi");
+        } else {
+          _.guess = null;
+          _.paused = true;
+          setTimeout(function () {
+            $(".picked").removeClass("picked");
+            Memory.paused = false;
+          }, 600);
+        }
+        if ($(".matched").length == $(".card").length) {
+          _.win();
+        }
+      }
+    },
+
+    win: function () {
+      this.paused = true;
+      setTimeout(function () {
+        Memory.showModal();
+        Memory.$game.fadeOut();
+      }, 1000);
+    },
+
+    showModal: function () {
+      this.$overlay.show();
+      this.$modal.fadeIn("slow");
+    },
+
+    hideModal: function () {
+      this.$overlay.hide();
+      this.$modal.hide();
+    },
+
+    reset: function () {
+      this.hideModal();
+      this.shuffleCards(this.cardsArray);
+      this.setup();
+      this.$game.show("slow");
+    },
+
+    // Fisher--Yates Algorithm -- https://bost.ocks.org/mike/shuffle/
+    shuffle: function (array) {
+      var counter = array.length,
+        temp,
+        index;
+      // While there are elements in the array
+      while (counter > 0) {
+        // Pick a random index
+        index = Math.floor(Math.random() * counter);
+        // Decrease counter by 1
+        counter--;
+        // And swap the last element with it
+        temp = array[counter];
+        array[counter] = array[index];
+        array[index] = temp;
+      }
+      return array;
+    },
+
+    buildHTML: function () {
+      var frag = "";
+      this.$cards.each(function (k, v) {
+        frag += '<div class="card" data-id="' + v.id +'"><div class="inside">\
+				<div class="front"><img src="' +
+          v.img +
+          '"\
+				alt="' +
+          v.name +
+          '" /></div>\
+				<div class="back"><img src="yusuf2.gif"\
+				alt="Codepen" /></div></div>\
+				</div>';
+      });
+      return frag;
+    },
+  };
+
+  Memory.init(cards);
+})();
+
+function switcher() {
+  document.getElementById("fp").style.display = "none";
+  $("#wp").fadeIn("slow");
+  document.getElementById("wp").style.display = "block";
+}
+
+
+function refreshPage(){
+    window.location.reload();
+} 
+
+
